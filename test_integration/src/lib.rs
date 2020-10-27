726fc5730840da1f591d8dc67f7b1daaca9c1f6e
Lewin Bormann <lbo@spheniscida.de>
2020-10-27 11:19:00 +0100

    test_integration: Provide client secret

--- test_integration/src/lib.rs
+++ test_integration/src/lib.rs
@@ -1,11 +1,43 @@
 mod integration_test_v1_types;
 
+use async_google_apis_common as agac;
 use integration_test_v1_types as inttest;
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use super::*;
+    use mockito;
+    use tokio;
+
+    const CLIENT_ID: &str = "myclientid.apps._dev.borgac.net";
+    const CLIENT_SECRET: &str = "mysecret";
+    const PROJECT_ID: &str = "integration-test-243420";
+    const AUTH_PATH: &str = "/oauth2/";
+    const TOKEN_PATH: &str = "/token/";
+
+    fn url_for_path(path: &str) -> String {
+        if path.starts_with("/") {
+            return mockito::server_url() + path;
+        }
+        return mockito::server_url() + "/" + path;
+    }
+
+    async fn read_client_secret() -> agac::yup_oauth2::ApplicationSecret {
+        let mut appsec = agac::yup_oauth2::read_application_secret("client_secret.json")
+            .await
+            .unwrap();
+        appsec.client_id = CLIENT_ID.into();
+        appsec.client_secret = CLIENT_SECRET.into();
+        appsec.project_id = Some(PROJECT_ID.into());
+        appsec.auth_uri = url_for_path(AUTH_PATH);
+        appsec.token_uri = url_for_path(TOKEN_PATH);
+        appsec.auth_provider_x509_cert_url = None;
+        appsec
+    }
+
+    #[tokio::test]
+    async fn it_works() {
+        println!("{}", mockito::server_url());
+        println!("{:?}", read_client_secret().await);
     }
 }
